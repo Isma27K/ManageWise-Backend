c2798c2dc0abfd296c01c5d220bbd4b1aa187e5f
ismasalalu <binhelati@gmail.com>
2024-10-19 22:54:36 +0800

    hihiaha

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ if (process.env.NODE_ENV === 'production') {
 // =================================== WHERE API ROUTES START =====================================================
 
 app.get('/', (req, res) => {
-  res.jsonp({message: "Hello, from izzma llagi. haloooo. test test"});
+  res.jsonp({message: "Hello, from izzma llagi. haloooo. bend bend"});
 });
 
 //app.use("/test", test);
