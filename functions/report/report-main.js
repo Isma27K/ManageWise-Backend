b9679b9d06774dd3ea0309ddcab64580ec3c931c
ismasalalu <binhelati@gmail.com>
2024-10-20 15:56:39 +0800

    save

--- functions/report/report-main.js
+++ functions/report/report-main.js
@@ -1,8 +1,20 @@
-const { poolTaskPartion } = require('./poolTaskPartion/poolTaskParttion.js');
+const { poolTaskPartion } = require('./poolTaskPartion/poolTaskParttion');
 
-const reportTask = async (req, res) => {
-    const value = await poolTaskPartion(req, res);
-    res.status(200).json({ value });
-}
+const reportMain = async (req, res) => {
+    try {
+        const poolTaskPartionResult = await poolTaskPartion(req, res);
+        
+        res.status(200).json({
+            success: true,
+            ...poolTaskPartionResult
+        });
+    } catch (error) {
+        console.error('Error in reportMain:', error);
+        res.status(500).json({
+            success: false,
+            message: 'An error occurred while generating the report'
+        });
+    }
+};
 
-module.exports = { reportTask };
+module.exports = { reportMain };
