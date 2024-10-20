a346f37ea0680b3910420ad1f099f1934c3c60fb
ismasalalu <binhelati@gmail.com>
2024-10-20 15:59:15 +0800

    save

--- routes/report/report.js
+++ routes/report/report.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../../middleware/jwtAuth.js');
-const { reportTask } = require('../../functions/report/report-main.js');
-
+const { reportMain } = require('../../functions/report/report-main.js');
 
 router.post('/report', authenticateToken, async (req, res) => {
-    return reportTask(req, res);
+    return reportMain(req, res);
 });
-
-
-
-
 
 module.exports = router;
