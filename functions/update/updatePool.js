80c58a5997aea36a8206c25a013e752a4467f21a
ismasalalu <binhelati@gmail.com>
2024-10-28 17:09:10 +0800

    save

--- functions/update/updatePool.js
+++ functions/update/updatePool.js
@@ -2,7 +2,7 @@ const Mongob = require('../../utils/mongodb/mongodb.js');
 const { ObjectId } = require('mongodb');
 
 const updatePool = async (req, res) => {
-    const { poolId, name, description, userIds } = req.body;
+    const { poolId, name, userIds } = req.body;
     const user = req.user.uid;
     
     if (!req.user.admin) {
@@ -20,7 +20,6 @@ const updatePool = async (req, res) => {
                 {
                     $set: {
                         name: name,
-                        description: description || '',
                         userIds: userIds || []
                     }
                 }
